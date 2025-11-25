9aef7b2b7ee2fc3fce64153e67e93d7531ed7d8f
saurabh <saurabhsharma708@gmail.com>
2025-11-25 23:17:44 +0530

    refactor: Rename query parameter in searchMovies
    This commit renames the parameter of the `searchMovies` method in the `MovieRepo` interface from `query3` to `query4`.

--- app/src/main/java/com/example/moviepractise2/domain/MovieRepo.kt
+++ app/src/main/java/com/example/moviepractise2/domain/MovieRepo.kt
@@ -4,5 +4,5 @@ import com.example.moviepractise2.data.remote.model.Movie
 import kotlinx.coroutines.flow.Flow
 
 interface MovieRepo {
-     fun  searchMovies(query3: String) : Flow<List<Movie>>
+     fun  searchMovies(query4: String) : Flow<List<Movie>>
 }
